747c15fe319ac1a2d1965c37a5140d6c672d5987
Lokrip <165576212+Lokrip@users.noreply.github.com>
2025-02-28 21:24:23 +0600

    chore: add coverage

---
+++ server/store/tests/test_serializers.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+
+from store.models import Book
+from store.serializer import BooksSerializer
+
+class BookSerializerTestCase(TestCase):
+    def test_ok(self):
+        book1 = Book.objects.create(name="Test book 1", price=25)
+        book2 = Book.objects.create(name="Test book 1", price=55) 
+        serializer = BooksSerializer([book1, book2], many=True)
+        expected_data = [
+            {
+                'id': book1.id,
+                'name': "Test book 1",
+                'price': f"{25:.2f}", 
+                'image': None
+            },
+            {
+                'id': book2.id,
+                'name': "Test book 1",
+                'price': f"{55:.2f}",
+                'image': None
+            }
+        ]
+        self.assertEqual(expected_data, serializer.data)
