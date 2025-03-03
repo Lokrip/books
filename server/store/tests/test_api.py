7e60afb94da39559bc71d33ab1a90b51c175a135
Lokrip <165576212+Lokrip@users.noreply.github.com>
2025-03-03 22:30:27 +0600

    test: add test to filters

--- server/store/tests/test_api.py
+++ server/store/tests/test_api.py
@@ -7,17 +7,35 @@ from store.models import Book
 from store.serializer import BooksSerializer
 
 class BooksApiTestCase(APITestCase):
-    def test_get(self):
+    #эта функция запускаеться каждый раз перед тем как запускаешь какойта из наших тестов
+    def setUp(self): 
         # Django автоматически создает базу данных с таблицей Book,
         # затем добавляет туда данные, а по окончании теста удаляет базу данных с таблицами.
-        book1 = Book.objects.create(name="Test book 1", price=25)
-        book2 = Book.objects.create(name="Test book 1", price=55)
+        self.book1 = Book.objects.create(name="Test book 1", price=25, author_name="Author 1")
+        self.book2 = Book.objects.create(name="Test book 2", price=55, author_name="Author 5")
+        self.book3 = Book.objects.create(name="Test book Author 1", price=55, author_name="Author 2")
+        
+    def test_get(self):
+        # # Django автоматически создает базу данных с таблицей Book,
+        # # затем добавляет туда данные, а по окончании теста удаляет базу данных с таблицами.
+        # book1 = Book.objects.create(name="Test book 1", price=25)
+        # book2 = Book.objects.create(name="Test book 1", price=55)
         
         #через reverse мы берем name="book-list" тоесть имя маршрута
         #например path("api/books", book_list, name="book-list") берем name
         url = reverse("book-list")
         response = self.client.get(url)
         
-        serializer = BooksSerializer([book1, book2], many=True)
+        serializer = BooksSerializer([self.book1, self.book2, self.book3], many=True)
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(serializer.data, response.data)
+        
+    def test_get_filter(self):
+        #через reverse мы берем name="book-list" тоесть имя маршрута
+        #например path("api/books", book_list, name="book-list") берем name
+        url = reverse("book-list")
+        response = self.client.get(url, data={"search": "Author 1"})
+        
+        serializer = BooksSerializer([self.book1, self.book3], many=True)
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(serializer.data, response.data)
